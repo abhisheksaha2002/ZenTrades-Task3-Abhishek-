367e33282645192de432cdfaf4c8f54071977191
Abhishek Saha <84782975+abhisheksaha2002@users.noreply.github.com>
2024-01-09 20:14:17 +0530

    Add files via upload

---
+++ src/Assets/hidepass.svg.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HidePass = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M2 12a10 10 0 1120 0 10 10 0 01-20 0z"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default HidePass;
---
+++ src/Assets/showpass.svg.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ShowPass = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default ShowPass;
